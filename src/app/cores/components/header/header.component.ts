3d8762011be298234e50e71fc20fe3fa262671ed
KONE HADOU <hadoukone0102@gmail.com>
2024-11-25 23:48:54 +0000

    une version standard

--- src/app/cores/components/header/header.component.ts
+++ src/app/cores/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  currentLanguage = 'English'; // Par défaut en anglais
+
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('lang') || 'en'; // Langue par défaut : anglais
+    this.translate.setDefaultLang(savedLang);
+    this.translate.use(savedLang);
+    this.currentLanguage = savedLang === 'en' ? 'English' : 'Français';
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+    this.currentLanguage = lang === 'en' ? 'English' : 'Français';
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
