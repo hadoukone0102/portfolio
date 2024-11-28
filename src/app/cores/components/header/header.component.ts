2b2584f6fb43c21f11e578b29427377843a68828
KONE HADOU <hadoukone0102@gmail.com>
2024-11-28 00:47:05 +0000

    une version de base

--- src/app/cores/components/header/header.component.ts
+++ src/app/cores/components/header/header.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import {TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
 export class HeaderComponent implements OnInit{
-  currentLanguage = 'English'; // Par défaut en anglais
 
   constructor(private translate: TranslateService) {
-    const savedLang = localStorage.getItem('lang') || 'en'; // Langue par défaut : anglais
-    this.translate.setDefaultLang(savedLang);
-    this.translate.use(savedLang);
-    this.currentLanguage = savedLang === 'en' ? 'English' : 'Français';
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const lang = localStorage.getItem('lang') || 'en';
+      this.translate.use(lang);
+    } else {
+      // Définit une langue par défaut si localStorage n'est pas disponible
+      this.translate.use('en');
+    }
   }
+  
 
   changeLanguage(lang: string) {
     this.translate.use(lang);
     localStorage.setItem('lang', lang);
-    this.currentLanguage = lang === 'en' ? 'English' : 'Français';
   }
 
   ngOnInit(): void {
