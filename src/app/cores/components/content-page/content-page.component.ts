e95686a7a58574de0e41860a9d159f4908a549ed
KONE HADOU <hadoukone0102@gmail.com>
2024-11-28 19:24:15 +0000

    content terminer

--- src/app/cores/components/content-page/content-page.component.ts
+++ src/app/cores/components/content-page/content-page.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-content-page',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule,CommonModule],
   templateUrl: './content-page.component.html',
   styleUrl: './content-page.component.scss'
 })
