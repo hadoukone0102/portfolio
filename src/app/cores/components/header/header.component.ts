563918333c05a2bf991dd25d852257112fcfbc48
KONE HADOU <hadoukone0102@gmail.com>
2024-11-28 17:11:22 +0000

    header terminer

--- src/app/cores/components/header/header.component.ts
+++ src/app/cores/components/header/header.component.ts
@@ -14,20 +14,30 @@ export class HeaderComponent implements OnInit{
     if (typeof window !== 'undefined' && window.localStorage) {
       const lang = localStorage.getItem('lang') || 'en';
       this.translate.use(lang);
+      this.currentLanguage = lang;
     } else {
       // Définit une langue par défaut si localStorage n'est pas disponible
       this.translate.use('en');
     }
   }
   
+  currentLanguage: string = 'en';
+  menuActive: boolean = false;
 
   changeLanguage(lang: string) {
     this.translate.use(lang);
     localStorage.setItem('lang', lang);
+    this.currentLanguage = lang;
   }
+
 
   ngOnInit(): void {
     
+  }
+
+  toggleMenu(): void {
+    const headerSecond = document.querySelector('.header-second') as HTMLElement;
+    headerSecond.classList.toggle('active');
   }
 
 }
