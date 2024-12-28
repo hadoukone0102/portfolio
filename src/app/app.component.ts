7d055de65e1a1596329b682e3aa032f88d4c9a9e
KONE HADOU <hadoukone0102@gmail.com>
2024-12-28 21:29:15 +0000

    changement de methode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { LoaderService } from './cores/services/loader.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -14,7 +15,30 @@ export class AppComponent implements OnInit {
   title = 'Portfolio';
   private isFirstLoad = true; // Indicateur pour le premier chargement
 
-  constructor(public loaderService: LoaderService, private router: Router) {}
+  constructor(
+    public loaderService: LoaderService, 
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const lang = localStorage.getItem('lang') || 'en';
+      this.translate.use(lang);
+      this.currentLanguage = lang;
+    } else {
+      // Définit une langue par défaut si localStorage n'est pas disponible
+      this.translate.use('en');
+    }
+  }
+
+  currentLanguage: string = 'en';
+  menuActive: boolean = false;
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+    this.currentLanguage = lang;
+  }
+
 
   ngOnInit(): void {
     this.executeLoaderActions(); // Exécution lors du premier chargement
